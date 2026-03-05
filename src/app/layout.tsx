0f5b94cad64e1b87c35de08088bf664cab1e437a
CzernikSlawek <adres_emailczernik.slawomir2@gmail.com>
2026-03-05 13:31:10 +0100

    fix: next 15.5.12 compatibility and session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,51 +3,53 @@ import "./globals.css";
 import Header from "@/components/Layout/Header";
 import Footer from "@/components/Layout/Footer";
 import { ThemeProvider } from "next-themes";
-import ScrollToTop from '@/components/ScrollToTop';
+import ScrollToTop from "@/components/ScrollToTop";
 import Aoscompo from "@/utils/aos";
 import { DonationProvider } from "./context/donationContext";
 import SessionProviderComp from "@/components/nextauth/SessionProvider";
 import { AuthDialogProvider } from "./context/AuthDialogContext";
-const montserrat = Montserrat({ subsets: ["latin"] });
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
 import Script from "next/script";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-  session,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-  session:any
-}>) {
+}) {
   return (
     <html lang="pl" suppressHydrationWarning>
       <body className={montserrat.className}>
-      <NextTopLoader color="#FF4D7E" />
-      <DonationProvider>
-        <AuthDialogProvider>
-      <SessionProviderComp session={session}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={true}
-          defaultTheme="system"
-        >
-          <Aoscompo>
-            <Header />
-            
-            {children}
-            
-            <Footer />
-          </Aoscompo>
-          <ScrollToTop />
-        </ThemeProvider>
-        </SessionProviderComp>
-        </AuthDialogProvider>
+        <NextTopLoader color="#FF4D7E" />
+
+        <DonationProvider>
+          <AuthDialogProvider>
+            <SessionProviderComp>
+              <ThemeProvider
+                attribute="class"
+                enableSystem={true}
+                defaultTheme="system"
+              >
+                <Aoscompo>
+                  <Header />
+
+                  {children}
+
+                  <Footer />
+                </Aoscompo>
+
+                <ScrollToTop />
+              </ThemeProvider>
+            </SessionProviderComp>
+          </AuthDialogProvider>
         </DonationProvider>
+
         <Script
-  src="https://cdn.userway.org/widget.js"
-  data-account="FBKFSL0XvR"
-  strategy="afterInteractive"
-/>
+          src="https://cdn.userway.org/widget.js"
+          data-account="FBKFSL0XvR"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
